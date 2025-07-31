77e23b8b4e812280bfbdc42f0a5e576d1ba5b891
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-08-01 00:58:39 +0600

    added Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,18 @@
+import Footer from "./components/Footer/Footer";
+import Navigation from "./components/Navigations/Navigation";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="">
+        <Navigation></Navigation>
+        <div className="max-w-[1280px] mx-auto">
+          {children}
+        </div>
+        <div>
+          <Footer></Footer>
+        </div>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Navigation from "./components/Navigation";
+
 
 export default function Home() {
   return (
     <div className="">
-      <Navigation></Navigation>
+
       <h1 className="text-3xl font-bold">Car Exparts Projects</h1>
+
     </div>
   );
 }
